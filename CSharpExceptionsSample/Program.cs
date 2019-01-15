364439badf72054a8797141f45a9c9d3080b9081
Eric J Fisher <eric-fisher@pluralsight.com>
2019-01-15 11:02:32 -0500

    Resubmitted CSharpExceptions Sample

--- CSharpExceptionsSample/Program.cs
+++ CSharpExceptionsSample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlClient;
 
 namespace CSharpExceptionsSample
 {
@@ -8,46 +7,6 @@ namespace CSharpExceptionsSample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
-
-        public static void Payment(int amount)
-        {
-            if(amount <= 0)
-            {
-                throw new ArgumentException("Payments cannot be zero or less");
-            }
-        }
-
-        public static void UpdateUser()
-        {
-            try
-            {
-                UpdateUserData();
-            }
-            catch(SqlException ex)
-            {
-                Log.AlertEveryone(ex);
-                throw;
-            }
-            catch(Exception ex)
-            {
-                Log.Error(ex);
-                throw;
-            }
-            finally
-            {
-                DisposeOfDatabaseConnection();
-            }
-        }
-
-        public static void UpdateUserData()
-        {
-
-        }
-
-        public static void DisposeOfDatabaseConnection()
-        {
-
         }
     }
 }
