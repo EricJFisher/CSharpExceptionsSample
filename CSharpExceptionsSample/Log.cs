2899e2ed0cf455742eed8de895654b47d60fbc5c
Eric J Fisher <eric-fisher@pluralsight.com>
2019-01-15 10:55:43 -0500

    Remaking of CSharpExceptionsSample project

---
+++ CSharpExceptionsSample/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExceptionsSample
+{
+    public static class Log
+    {
+        public static void AlertEveryone(Exception ex)
+        {
+
+        }
+
+        public static void Error(Exception ex)
+        {
+
+        }
+    }
+}
---
+++ CSharpExceptionsSample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CSharpExceptionsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Payment(int amount)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentException("Payments cannot be zero or less");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            try
+            {
+                UpdateUserData();
+            }
+            catch(SqlException ex)
+            {
+                Log.AlertEveryone(ex);
+                throw;
+            }
+            catch(Exception ex)
+            {
+                Log.Error(ex);
+                throw;
+            }
+            finally
+            {
+                DisposeOfDatabaseConnection();
+            }
+        }
+
+        public static void UpdateUserData()
+        {
+
+        }
+
+        public static void DisposeOfDatabaseConnection()
+        {
+
+        }
+    }
+}
